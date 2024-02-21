d0c1935aa3a682e7f878cad67b05b838bb299958
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-02-21 13:46:35 +0100

    creating a function that takes all images in an array and assignes ids to them for css manipulation

--- src/importImages.js
+++ src/importImages.js
@@ -1,9 +1,15 @@
 import createImg from "./createImg";
-import pies from "./img"
 
 
-export default function importImages(){
-    const pieImgs = pies;
+export default function importImages(imgArray){
+    const allImgs = imgArray;
+    const configuredImgs = []
+
     let imgNum = 0
-    
+    allImgs.forEach(img => {
+        const configuredImg = createImg(img, 'images', `pie${imgNum}`)
+        configuredImgs.push(configuredImg)
+        imgNum++
+    });
+    return configuredImgs
 }
